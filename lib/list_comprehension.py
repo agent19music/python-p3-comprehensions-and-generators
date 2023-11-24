97607c5165b2306ddff983541beeb0cc415ccd3f
agent19music <seanmotanya@gmail.com>
2023-11-24 21:55:31 +0300

    Done with assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens_list = [n for n in num_list if n%2 == 0]
+    return evens_list
     pass
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) > 0:
+     exclamation_list = [s + "!" for s in sentence_list]
+     return exclamation_list
+    else :
+     return sentence_list
+     pass
